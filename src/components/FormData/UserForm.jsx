f1d181fac66776a8f6c813aec3dbc50cce89a234
Aamna Majid <aamna.majid@gmail.com>
2021-03-13 15:51:07 +0500

    fixed

--- src/components/FormData/UserForm.jsx
+++ src/components/FormData/UserForm.jsx
@@ -225,7 +225,6 @@ export class UserForm extends Component {
       contactPref,
       contactMethodPref,
       marketingMethodPref,
-      date: Date.now(),
       collegeName: randomElement,
     };
 
