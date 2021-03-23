dfbf9ec328b21c64f45e1b75f3fb6d7ea7dee287
Aamna Majid <aamna.majid@gmail.com>
2021-03-23 22:32:32 +0500

    core ui table

--- src/common/formData.js
+++ src/common/formData.js
@@ -1,27 +1,36 @@
-import React, { useState , useEffect} from 'react';
-import ListGroup from './listGroup'
-import Pagination from './pagination'
-import {paginate} from './paginate'
-import axios from 'axios'
-import _ from 'lodash'
+import React from 'react';
+import { CButton,  CDataTable } from "@coreui/react";
+
 const Formdata=(props)=>{
-    const {posts} = props;
-    //added search
-    const [search,setSearch]=useState();
-    //-------------------
-    const cities= ["All","London","Lahore"]
-    const [city, setCurrentCity]= useState(cities)
-    const [s_date, setDate] = useState()
-    const [currentPage,setcurrentPage]=useState(1);
-    const PageSize = 10;
-  const [sortColumn,setSortColumn] = useState({path: 'city', order:'asc'})
-    if(!posts || posts.length===0) return <p>Cannot find any posts</p>;
+  const { posts } = props;
+  const arr = [];
+  //converting post data into array
+  const obj = Object.entries(posts.data);
+  obj.forEach(([key, value]) => arr.push(value));
+  console.log(arr);
   
-      var filtered = city && city!=="All"? posts.data.filter(m => m.city === city): posts.data;
-    
-     const sorted = _.orderBy(filtered,[sortColumn.path],[sortColumn.order])
-    
-     const forms = paginate(filtered, currentPage , PageSize);
+   
+    if(!posts || posts.length===0) return <p>Cannot find any posts</p>;
+    const fields = [
+      { key: "name", _style: { width: "40%" } },
+      "courseTitle",
+      {
+        key: "buttons",
+        label: "",
+        _style: { width: "1%" },
+        sorter: false,
+        filter: false,
+      },
+      {
+        key: "button1",
+        label: "",
+        _style: { width: "1%" },
+        sorter: false,
+        filter: false,
+      },
+    ];
+  
+     
     
     
     
@@ -71,109 +80,62 @@ const Formdata=(props)=>{
         });
      }
     
-    const handleCitySelect=city=>{
-     setCurrentCity(city)
-     
-    }
-   
+     return (
+      <CDataTable
+        items={arr}
+        fields={fields}
+        columnFilter
+        tableFilter
+        footer
+        itemsPerPageSelect
+        itemsPerPage={5}
+        hover
+        sorter
+        pagination
+        scopedSlots={{
+          buttons: (item, index) => {
+            return (
+              <td className="py-2">
+                <CButton
+                  color="primary"
+                  variant="outline"
+                  shape="square"
+                  size="sm"
+                  onClick={() => {
+                    this.handleChange(item._id);
+                  }}
+                >
+                  Fill PDF
+                </CButton>
+              </td>
+            );
+          },
+          button1: (item, index) => {
+            return (
+              <td className="py-2">
+                <CButton
+                  color="primary"
+                  variant="outline"
+                  shape="square"
+                  size="sm"
+                  onClick={() => {
+                    console.log("clicked");
+                  }}
+                >
+                  Sign
+                </CButton>
+              </td>
+            );
+          },
+        }}
+      />
+    );
 
- const handlePageChange=page=>{
-     
-    setcurrentPage(page)
-    
- }
- const handleSort = path=>{
-   setSortColumn({path, order:'asc'})
- }
 
-    return(
      
        
       
-      <div className="  row">
-        
-        
-      <br />
-         
-      <div className="col-2">
-      <h5>Filter by City:</h5>
-      <ListGroup items={cities} onItemSelect={handleCitySelect} selectedItem={city} /><br/>
     
-    
-    </div>
-            
-            
-            <div className="col">
-            
-            <br/>
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th>Student Name</th>
-                        <th  onClick={()=>handleSort('city')}>City</th>
-                        <th onClick={()=>handleSort('title')}>Course Title</th>
-                        <th>Date</th>
-                        <th></th>
-                        <th></th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                {forms.map((post)=>{
-                  
-                    return(
-<tr>
-
-                        <td>{post.firstName}</td>
-                        <td>{post.city}</td>
-                        <td>{post.appliedCourse}</td>
-                        <td>{post.date}</td>
-                        <td><button className="btn btn-danger btn-sm" onClick={
-                           e=>{
-                            window.location.href="/show-data?id="+post._id}}>Show Data
-                          </button></td>
-                        <td><button className="btn btn-success btn-sm" onClick={
-                          ()=>{handleChange(post._id)}
-                        }>Fill PDF</button>
-                            
-                        </td>
-                        
-                        <td><button className="btn btn-warning btn-sm" onClick={
-                          ()=>{handleSignChange(post._id)}
-                        }>Sign PDF</button></td>
-                        
-                        <td>
-                        <select
-              name="wheelchair"
-              defaultValue="[Please select one]"
-              onChange={handleChange}
-            >
-              <option value="[Please select one]">[Please select one]</option>
-              <option value="Yes">College A</option>
-              <option value="No">College B</option>
-            </select>
-                        </td>
-                        
-                    </tr>
-                    )
-                })}
-                    
-                
-                </tbody>
-            </table>
-
-            </div>
-    
-
-
-<Pagination 
-itemsCount={filtered.length} 
-pageSize={10}
-onPageChange={handlePageChange}
-currentPage={currentPage}/>
-         </div>
-        
-    )
 }
 
 export default Formdata;
